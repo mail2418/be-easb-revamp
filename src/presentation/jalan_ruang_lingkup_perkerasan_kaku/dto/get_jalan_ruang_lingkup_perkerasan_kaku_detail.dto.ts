1e9083337f55fa17513d35b76148a04ccbf7b340
Muhammad22064 <muhammad.panjerino@btn.co.id>
2025-12-16 10:27:45 +0700

    fix: transform id type number in get detail dto

--- src/presentation/jalan_ruang_lingkup_perkerasan_kaku/dto/get_jalan_ruang_lingkup_perkerasan_kaku_detail.dto.ts
+++ src/presentation/jalan_ruang_lingkup_perkerasan_kaku/dto/get_jalan_ruang_lingkup_perkerasan_kaku_detail.dto.ts
@@ -1,7 +1,9 @@
 import { IsNotEmpty, IsNumber } from "class-validator";
+import { Transform } from "class-transformer";
 
 export class GetJalanRuangLingkupPerkerasanKakuDetailDto {
     @IsNotEmpty()
     @IsNumber()
+    @Transform(({ value }) => parseInt(value, 10))
     id!: number;
 }
