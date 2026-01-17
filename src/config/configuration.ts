0e72f6602efd05e9f5f63e57dad9e6b140f19974
Anggito02 <anggito_anju@yahoo.com>
2026-01-17 20:10:52 +0700

    [sec] add rate limiting

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -10,4 +10,12 @@ export default () => ({
         accessTtl: process.env.JWT_ACCESS_TTL || '8h',
         refreshTtl: process.env.JWT_REFRESH_TTL || '24h',
     },
+    throttler: {
+        ttl: parseInt(process.env.RATE_TIME_LIMIT ?? '60000', 10),
+        limit: parseInt(process.env.RATE_REQ_LIMIT ?? '100', 10),
+        auth: {
+            ttl: parseInt(process.env.RATE_LIMIT_AUTH_TTL ?? '60000', 10),
+            limit: parseInt(process.env.RATE_AUTH_LIMIT ?? '5', 10),
+        },
+    },
 });
